9256255cd80005371d15f2b90b1e3240f12ef049
gazoubel <gazoubel@gmail.com>
2016-04-04 23:06:18 -0400

    added total expense in project-stage

--- app/models/project-stage.js
+++ app/models/project-stage.js
@@ -8,4 +8,10 @@ export default DS.Model.extend({
   project: DS.belongsTo('project',   {async: true}),
   stage: DS.belongsTo('stage',   {async: true}),
   expenseItems: DS.hasMany('expense-item',   {async: true}),
+  totalExpense: (function() {
+    var expenseItems = this.get('expenseItems');
+    return expenseItems.reduce(function(prev, item) {
+      return (prev || 0) + Number(item.get('total'));
+    });
+  }).property('expenseItems.@each.total')
 });
