db997f547f61393ce4fd8dec93a0108679c5f87d
gazoubel <gazoubel@gmail.com>
2016-04-10 21:40:54 -0400

    improved power select code

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    'ember-power-select': {
+      theme: 'bootstrap'
+    }
   });
 
     app.import('bower_components/bootstrap/dist/css/bootstrap.css');
